3fbb3e8b1182b72270e87f00aa2566d31b90ec90
Chris Roberts <chrisroberts.code@gmail.com>
2015-01-19 15:10:37 -0800

    Provide overrides to support pulling configuration details from fission namespace

--- lib/fission-assets/store.rb
+++ lib/fission-assets/store.rb
@@ -1,3 +1,24 @@
 require 'fission-assets'
 
-Fission::Assets::Store = Jackal::Assets::Store
+module Fission
+  module Assets
+    class Store < Jackal::Assets::Store
+
+      # @return [String] name of bucket
+      def bucket_name
+        super ||
+          Carnivore::Config.get(:fission, :assets, :bucket)
+      end
+
+      # @return [Smash] connection arguments
+      def connection_arguments
+        result = super
+        if(result.empty?)
+          result = Carnivore::Config.fetch(:fission, :assets, :connection, result)
+        end
+        result
+      end
+
+    end
+  end
+end
